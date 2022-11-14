973507267584b0d22334d18156beca9ca17a307d
Michał Święciło <michal_00-00@o2.pl>
2022-11-14 02:06:03 +0100

    FPS Counter

--- main.py
+++ main.py
@@ -7,20 +7,32 @@ from constants import ARUCO_DICT
 
 
 def print_hi(name):
-    dict_name = "DICT_7X7_1000"
+    cv2.namedWindow("Preview")
+    cap = cv2.VideoCapture(1)
+    # used to record the time when we processed last frame
+    prev_frame_time = 0
+    # used to record the time at which we processed current frame
+    new_frame_time = 0
+    while True:
+        ret, frame = cap.read()
+        # time when we finish processing for this frame
+        new_frame_time = time.time()
+        # fps will be number of frame processed in given time frame
+        # since their will be most of time error of 0.001 second
+        # we will be subtracting it to get more accurate result
+        fps = 1 / (new_frame_time - prev_frame_time)
+        prev_frame_time = new_frame_time
 
-    aruco_dict = cv2.aruco.Dictionary_get(ARUCO_DICT[dict_name])
-    print(aruco_dict.bytesList)
-    print("--------------------------")
-    print(len(aruco_dict.bytesList))
+        fps = str(int(fps))
 
-    print(bool(1))
-
-    def none_or_str(value):
-        if value == 'None':
-            return None
-        return value
-
+        # putting the FPS count on the frame
+        cv2.putText(frame, fps, (7, 70), cv2.FONT_HERSHEY_SIMPLEX, 3, (100, 255, 0), 3, cv2.LINE_AA)
+        cv2.imshow("Preview", frame)
+        # press 'Q' if you want to exit
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
 
 
 # Press the green button in the gutter to run the script.
